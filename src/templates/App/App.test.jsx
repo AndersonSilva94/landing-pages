2a9c632415c1ffb7ed8e5661618b0416e0cb282e
Anderson Silva <andssilva.23@gmail.com>
2021-10-04 11:08:02 -0400

    feat: :rocket: configure Jest and jest-styled-components

--- src/templates/App/App.test.jsx
+++ src/templates/App/App.test.jsx
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
 import Home from '.';
+import { theme } from '../../styles/theme';
 
 test('renders learn react link', () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { getByRole } = renderTheme(<Home />);
+  const headingContainer = getByRole('heading', {
+    name: 'Hello World!',
+  }).parentElement;
+
+  expect(headingContainer).toHaveStyle({
+    background: theme.colors.mainBg,
+  });
+
+  expect(headingContainer).toMatchSnapshot();
+  expect(headingContainer).toHaveStyleRule('background', 'red');
 });
