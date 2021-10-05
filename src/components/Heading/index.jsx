64deb0c6827cfd05c3de1e0717f5a1eb31269c8b
Anderson Silva <andssilva.23@gmail.com>
2021-10-05 15:19:25 -0400

    feat: :rocket: configure storybook - part 2

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import * as Styled from './styles';
 
-function Heading({ children }) {
-  return <Styled.Title>{children}</Styled.Title>;
+function Heading({ children, light = false }) {
+  return <Styled.Title light={light}>{children}</Styled.Title>;
 }
 
 Heading.propTypes = {
   children: PropTypes.node.isRequired,
+  light: PropTypes.bool.isRequired,
 };
 
 export default Heading;
