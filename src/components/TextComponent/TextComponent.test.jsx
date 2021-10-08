ee4619049d24c1b36fff5173008f22ca4985e71f
Anderson Silva <andssilva.23@gmail.com>
2021-10-08 12:53:12 -0400

    feat: :rocket: test TextComponent

--- src/components/TextComponent/TextComponent.test.jsx
+++ src/components/TextComponent/TextComponent.test.jsx
@@ -1,9 +1,29 @@
 import { renderTheme } from '../../styles/render-theme';
-import { TextComponent } from '.';
+import TextComponent from '.';
+import { theme } from '../../styles/theme';
 
 describe('<TextComponent />', () => {
   it('should render', () => {
-    const { getByRole } = renderTheme(<TextComponent>Children</TextComponent>);
-    expect(getByRole('heading')).toBeInTheDocument();
+    const { getByText } = renderTheme(
+      <TextComponent>test component</TextComponent>,
+    );
+    expect(getByText(/test component/i)).toBeInTheDocument();
+  });
+  it('should have the defined style', () => {
+    const { getByText } = renderTheme(
+      <TextComponent>test component</TextComponent>,
+    );
+    const textElement = getByText(/test component/i);
+
+    expect(textElement).toHaveStyle({
+      'font-size': theme.font.size.medium,
+    });
+  });
+  it('should match snapshot', () => {
+    const { container } = renderTheme(
+      <TextComponent>test component</TextComponent>,
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
