1d25f0c26cdfbb296f4502f564c0c289abfd9a99
Anderson Silva <andssilva.23@gmail.com>
2021-10-08 13:26:04 -0400

    feat: :rocket: test MenuLink component

---
+++ src/components/MenuLink/MenuLink.test.jsx
@@ -0,0 +1,37 @@
+import { renderTheme } from '../../styles/render-theme';
+import MenuLink from '.';
+
+describe('<MenuLink />', () => {
+  it('should render menu link', () => {
+    const { getByRole } = renderTheme(
+      <MenuLink link="http://www.google.com">Children</MenuLink>,
+    );
+    const linkTest = getByRole('link', { name: /children/i });
+    expect(linkTest).toBeInTheDocument();
+  });
+  it('should render attributes', () => {
+    const { getByRole } = renderTheme(
+      <MenuLink link="http://www.google.com">Children</MenuLink>,
+    );
+    const linkTest = getByRole('link', { name: /children/i });
+    expect(linkTest).toHaveAttribute('target', '_self');
+  });
+  it('should open in a new tab', () => {
+    const { getByRole } = renderTheme(
+      <MenuLink link="http://www.google.com" newTab={true}>
+        Children
+      </MenuLink>,
+    );
+    const linkTest = getByRole('link', { name: /children/i });
+    expect(linkTest).toHaveAttribute('target', '_blank');
+  });
+  it('should match snapshot', () => {
+    const { getByRole } = renderTheme(
+      <MenuLink link="http://www.google.com" newTab={true}>
+        Children
+      </MenuLink>,
+    );
+    const linkTest = getByRole('link', { name: /children/i });
+    expect(linkTest).toMatchSnapshot();
+  });
+});
